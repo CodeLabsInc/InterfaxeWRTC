7bdf7fe8c65f829886cca035719e36cfa42925db
Muhammad Mehdi <arsalanjaf@gmail.com>
2021-11-19 11:18:37 +0500

    changes intrfaxe

--- Example/IntrfaxeWRTC/AppDelegate.swift
+++ Example/IntrfaxeWRTC/AppDelegate.swift
@@ -13,11 +13,104 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
+
+    
+//    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([Any]?) -> Void) -> Bool {
+//        print("Continue User Activity called: ")
+//        if userActivity.activityType == NSUserActivityTypeBrowsingWeb {
+//            let url = userActivity.webpageURL!
+//            print(url.absoluteString)
+//            //handle url and open whatever page you want to open.
+//        }
+//        return true
+//    }
+    
+    
+    
+    /// set orientations you want to be allowed in this property by default
+    var orientationLock = UIInterfaceOrientationMask.all
+
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+            return self.orientationLock
+    }
+    
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([Any]?) -> Void) -> Bool {
+
+        
+        print("****Function: \(#function), line: \(#line)****\n-- ")
+
+            guard userActivity.activityType == NSUserActivityTypeBrowsingWeb,
+                let url = userActivity.webpageURL, let components = URLComponents(url: url, resolvingAgainstBaseURL: true) else {
+                    return false
+                }
+        print("url \(url)")
+        print("components.path \(components.path)")
+        
+        let queryItems = URLComponents(string: url.absoluteString)?.queryItems
+        let param1 = queryItems?.filter({$0.name == "room"}).first
+        print(param1?.value ?? "")
+        
+        let vc = UIStoryboard.init(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "FirstViewController") as? FirstViewController
+        
+        
+        if (param1?.value != nil )
+        
+        {
+            let decodedData = Data(base64Encoded: param1?.value ?? "")!
+            let decodedString = String(data: decodedData, encoding: .utf8)!
+            
+            print(decodedString)
+            
+            let strArray = decodedString.components(separatedBy: "_")
+            print("roomID \(String(describing: strArray.last))")
+            print("roomName \(String(describing: strArray.first))")
+
+            let roomID = strArray.last ?? ""
+            let roomName = strArray.first ?? ""
+            
+            if (!roomID.isEmpty){
+                
+                
+                
+                vc?.roomID = roomID
+                vc?.roomName = roomName
+                
+                
+                
+                
+            }
+            
+            
+        }
+        let navigationController = UINavigationController(rootViewController: vc!)
+        
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        if let window = window {
+            window.rootViewController = navigationController
+        }
+        window?.makeKeyAndVisible()
+        
+        
+
+            //FOR A URL "https://yourwebsite.com/testing/24
+            //this will print the ID 24
+            
+//            if (components.path.contains("testing")) {
+//                if let theid = Int(url.lastPathComponent) {
+//                    print("test id from deep link \(theid)")
+//                }
+//            }
+            
+            
+            return false
+    }
+
+ 
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
--- IntrfaxeWRTC.podspec
+++ IntrfaxeWRTC.podspec
@@ -8,35 +8,42 @@
 
 Pod::Spec.new do |s|
   s.name             = 'IntrfaxeWRTC'
-  s.version          = '0.1.0'
-  s.summary          = 'A short description of IntrfaxeWRTC.'
-
-# This description is used to generate tags and improve search results.
-#   * Think: What does it do? Why did you write it? What is the focus?
-#   * Try to keep it short, snappy and to the point.
-#   * Write the description between the DESC delimiters below.
-#   * Finally, don't worry about the indent, CocoaPods strips it!
-
+  s.version          = '1.0.0'
+  s.summary          = 'Intrfaxe WebRTC '
+  
+  # This description is used to generate tags and improve search results.
+  #   * Think: What does it do? Why did you write it? What is the focus?
+  #   * Try to keep it short, snappy and to the point.
+  #   * Write the description between the DESC delimiters below.
+  #   * Finally, don't worry about the indent, CocoaPods strips it!
+  
   s.description      = <<-DESC
-TODO: Add long description of the pod here.
-                       DESC
-
-  s.homepage         = 'https://github.com/11611707/IntrfaxeWRTC'
+  TODO: Add long description of the pod here.
+  DESC
+  s.homepage         = 'https://github.com/CodeLabsInc/IntrfaxeWRTC.git'
   # s.screenshots     = 'www.example.com/screenshots_1', 'www.example.com/screenshots_2'
   s.license          = { :type => 'MIT', :file => 'LICENSE' }
-  s.author           = { '11611707' => 'arsalanjaf@gmail.com' }
-  s.source           = { :git => 'https://github.com/11611707/IntrfaxeWRTC.git', :tag => s.version.to_s }
+  s.author           = { 'Muhammad Mehdi' => 'm.mehdi@codelabs.inc' }
+  s.source           = { :git => 'https://github.com/CodeLabsInc/IntrfaxeWRTC.git', :tag => s.version.to_s }
   # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
-
-  s.ios.deployment_target = '9.0'
-
+  
+  s.ios.deployment_target = '10.0'
+  s.swift_versions = '4.2'
+  
   s.source_files = 'IntrfaxeWRTC/Classes/**/*'
   
   # s.resource_bundles = {
-  #   'IntrfaxeWRTC' => ['IntrfaxeWRTC/Assets/*.png']
+  #   'CLWebRTC' => ['CLWebRTC/Assets/*.png']
   # }
-
+  
   # s.public_header_files = 'Pod/Classes/**/*.h'
   # s.frameworks = 'UIKit', 'MapKit'
   # s.dependency 'AFNetworking', '~> 2.3'
+  s.frameworks = 'UIKit'
+  s.dependency 'Starscream', '3.0.6'
+  s.dependency 'WebRTC', '63.11.20455'
+  s.pod_target_xcconfig = {
+    'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'arm64'
+  }
+  s.user_target_xcconfig = { 'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'arm64' }
 end
